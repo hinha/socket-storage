aa629ef8d301752a1e964e3a2bebd2760cfe8ab7
hinha <martinuz.dawan9@gmail.com>
2020-11-28 20:02:02 +0700

    configure max_size 25

--- platform/grpc.go
+++ platform/grpc.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
+	"time"
 )
 
 type ConnectionsGrpc interface {
@@ -25,14 +27,29 @@ func InitializeGrpc(address, port, domain string) ConnectionsGrpc {
 	}
 }
 
+const MaxSizeFile = 1024 * 1024 * 25 // 25mb in bytes
+
 func (cs *connectionStringRpc) Open() *grpc.ClientConn {
+	var alive = keepalive.ClientParameters{
+		Time:                10 * time.Second, // send pings every 10 seconds if there is no activity
+		Timeout:             time.Second,      // wait 1 second for ping back
+		PermitWithoutStream: true,             // send pings even without active streams
+	}
+
 	logrus.WithFields(logrus.Fields{
 		"platform": "gRPC",
 		"domain":   cs.domain,
 	}).Info("Connection rpc")
 
 	portLine := fmt.Sprintf("%s:%s", cs.address, cs.port)
-	client, err := grpc.Dial(portLine, grpc.WithInsecure(), grpc.WithBlock())
+	logrus.Info(portLine)
+
+	client, err := grpc.Dial(portLine,
+		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(MaxSizeFile)),
+		grpc.WithInsecure(),
+		grpc.WithKeepaliveParams(alive),
+		grpc.WithBlock())
+
 	if err != nil {
 		logrus.WithFields(logrus.Fields{
 			"connection": cs.address,
