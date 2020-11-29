7c3d959572ee5c0bf821ffda0c05655bafca08b0
hinha <martinuz.dawan9@gmail.com>
2020-11-29 14:59:06 +0700

    logger

---
+++ log/log.go
@@ -0,0 +1,55 @@
+package log
+
+import (
+	"github.com/sirupsen/logrus"
+	"os"
+)
+
+type Event struct {
+	id      int
+	message string
+}
+
+type StandardLogger struct {
+	*logrus.Logger
+}
+
+var (
+	invalidArgMessage      = Event{1, "Invalid arg: %s"}
+	invalidArgValueMessage = Event{2, "Invalid value for argument: %s: %v"}
+	missingArgMessage      = Event{3, "Missing arg: %s"}
+)
+
+func NewLogger() *StandardLogger {
+	var baseLogger = logrus.New()
+	baseLogger.Out = os.Stdout
+
+	//hook, err := lSyslog.NewSyslogHook("", "", syslog.LOG_INFO, "")
+	//
+	//if err == nil {
+	//	baseLogger.Hooks.Add(hook)
+	//}
+
+	//file, err := os.OpenFile("log.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	//if err != nil {
+	//	panic(err)
+	//}
+
+	var standardLogger = &StandardLogger{baseLogger}
+	standardLogger.Formatter = &logrus.JSONFormatter{}
+	return standardLogger
+}
+
+func (l *StandardLogger) InvalidArg(argumentName string) {
+	l.Errorf(invalidArgMessage.message, argumentName)
+}
+
+// InvalidArgValue is a standard error message
+func (l *StandardLogger) InvalidArgValue(argumentName string, argumentValue error) {
+	l.Errorf(invalidArgValueMessage.message, argumentName, argumentValue)
+}
+
+// MissingArg is a standard error message
+func (l *StandardLogger) MissingArg(argumentName string) {
+	l.Errorf(missingArgMessage.message, argumentName)
+}
